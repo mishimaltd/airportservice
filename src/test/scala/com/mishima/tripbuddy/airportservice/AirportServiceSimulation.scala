ed08b92392228669942136315be56932cc82e4e3
Pugh, Joe <joe.pugh@cengage.com>
2018-06-18 23:36:28 -0400

    Created

--- src/test/scala/com/mishima/tripbuddy/airportservice/AirportServiceSimulation.scala
+++ src/test/scala/com/mishima/tripbuddy/airportservice/AirportServiceSimulation.scala
@@ -17,7 +17,12 @@ class AirportServiceSimulation extends Simulation {
         .get("/airports/code/RDU")
         .check(status.is(200)))
 
-  setUp(scn.inject(rampUsers(300) over (30 seconds))).protocols(httpConf)
+  setUp(scn.inject(
+    nothingFor(4 seconds),
+    atOnceUsers(10),
+    rampUsers(30) over (5 seconds),
+    constantUsersPerSec(35) during (90 seconds) randomized))
+    .protocols(httpConf)
     .assertions(
       global.failedRequests.count.is(0),
       global.responseTime.percentile3.lte(1200), //check that 95% of the response time is under 1200ms
